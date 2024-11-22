636497d2ccfeaa30008ef6aa48bb076879ef766e
AkshayJ <akshay.joshimit@gmail.com>
2024-11-22 20:27:52 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        //System.out.println(args[0]);
+        //System.out.println(args[1]);
+        ArrayList<Integer> array1 = new ArrayList<Integer>();
+        //System.out.println(args.length);
+        //System.out.println(args);
+        System.out.println("Hello and welcome! All the best");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
